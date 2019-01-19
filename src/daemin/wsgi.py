28004ac797984f7b31e8aca59fb51e62f23b046c
Miniminis <minhee4735@gmail.com>
2019-01-19 06:58:18 +0300

    Added signup page, but had NotFoundError

--- src/daemin/wsgi.py
+++ src/daemin/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daemin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baemin.settings')
 
 application = get_wsgi_application()
--- src/partner/views.py
+++ src/partner/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    ctx = {}
+    return render(request, "index.html", ctx)
+
+def signup(request):
+    ctx = {}
+    return render(request, "signup.html", ctx)
