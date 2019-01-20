0fc479150c9d5d094a22a8853531ba16aca06128
Miniminis <minhee4735@gmail.com>
2019-01-20 14:25:21 +0300

    Completed signup page

--- src/partner/views.py
+++ src/partner/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,5 +7,16 @@ def index(request):
     return render(request, "index.html", ctx)
 
 def signup(request):
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        # print(username, email, password)
+
+        user = User.objects.create_user(username, email, password)
+        #Article.objects.creat(title", content="")
+
     ctx = {}
     return render(request, "signup.html", ctx)
